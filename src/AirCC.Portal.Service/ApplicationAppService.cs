26456fc70e964e68085f4431905449f74cf6f140
ellefry <ellefry@qq.com>
2021-02-16 23:31:34 +0800

    update DeleteConfiguration

--- src/AirCC.Portal.Service/ApplicationAppService.cs
+++ src/AirCC.Portal.Service/ApplicationAppService.cs
@@ -1,14 +1,17 @@
-﻿using AirCC.Portal.AppService.Abstract;
+﻿using AirCC.Client.Registry;
+using AirCC.Portal.AppService.Abstract;
 using AirCC.Portal.AppService.ApplicationDtos;
 using AirCC.Portal.Domain;
 using AirCC.Portal.Domain.DomainServices;
 using BCI.Extensions.DDD.ApplicationService;
 using BCI.Extensions.Domain;
 using JetBrains.Annotations;
+using Microsoft.Extensions.Caching.Memory;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 using IApplicationService = AirCC.Portal.Domain.DomainServices.IApplicationService;
 
 namespace AirCC.Portal.AppService
@@ -17,13 +20,16 @@ namespace AirCC.Portal.AppService
     {
         private readonly IApplicationService applicationService;
         private readonly IRepository<ApplicationConfiguration, string> configurationRepository;
+        private readonly IMemoryCache memoryCache;
 
         public ApplicationAppService(IRepository<Application, string> repository, IServiceProvider serviceProvider,
-            IApplicationService applicationService, IRepository<ApplicationConfiguration, string> configurationRepository)
+            IApplicationService applicationService, IRepository<ApplicationConfiguration, string> configurationRepository, 
+            IMemoryCache memoryCache)
             : base(repository, serviceProvider)
         {
             this.applicationService = applicationService;
             this.configurationRepository = configurationRepository;
+            this.memoryCache = memoryCache;
         }
 
         public async Task Create([NotNull] ApplicationInput applicationInput)
@@ -57,20 +63,33 @@ namespace AirCC.Portal.AppService
             await applicationService.OnlineConfigurations(input.ConfigurationIds);
         }
 
-        public async Task OnlinConfiguration(string id)
+        public async Task OnlinConfiguration([NotNull] string id)
         {
             await applicationService.OnlineConfigurations(new List<string> { id });
         }
 
-        public async Task RevertConfiguration(string historyId)
+        public async Task RevertConfiguration([NotNull] string historyId)
         {
             await applicationService.RevertConfiguration(historyId);
         }
 
-        public async Task DeleteConfiguration(string id)
+        public async Task DeleteConfiguration([NotNull]string id)
         {
-            await configurationRepository.DeleteAsync(id);
+            //await configurationRepository.DeleteAsync(id);
             //update settings
+            var configuration = await configurationRepository.FindAsync(id);
+            var application = await Repository.FindAsync(configuration.ApplicationId);
+            if (memoryCache.TryGetValue(application.Name, out ApplicationRegistry app))
+            {
+
+                using var scope = new TransactionScope(scopeOption: TransactionScopeOption.Required,
+                    transactionOptions: new TransactionOptions { IsolationLevel = IsolationLevel.RepeatableRead },
+                        asyncFlowOption: TransactionScopeAsyncFlowOption.Enabled);
+                configurationRepository.Delete(id);
+
+                //send request
+                scope.Complete();
+            }
         }
     }
 }
