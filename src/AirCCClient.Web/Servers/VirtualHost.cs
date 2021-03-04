ad6f45c05a0ab3d69ed44e4091f65c274cfee8fe
wentie song <ellefry@live.cn>
2021-03-04 17:29:20 +0800

    hostbuilder

---
+++ src/AirCCClient.Web/Servers/VirtualHost.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace AirCCClient.Web.Servers
+{
+   
+    public class VirtualHost
+    {
+        private const string _appsettings = "appsettings.json";
+        private const string _hostsettings = "hostsettings.json";
+
+        public static async Task CreateHost()
+        {
+            var host = new HostBuilder()
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    configHost.AddJsonFile(_hostsettings, optional: true);
+                })
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.SetBasePath(Directory.GetCurrentDirectory());
+                    configApp.AddJsonFile(_appsettings, optional: true);
+                    configApp.AddJsonFile(
+                        $"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
+                        optional: true);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddLogging();
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddConsole();
+                })
+                .UseConsoleLifetime()
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
